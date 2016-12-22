9e434dcf3fd9d877cc7f17fa75d6d607377a6e02
N33l <neelendrakmr3@gmail.com>
2016-12-22 21:45:52 +0530

    setup to run in heroku

---
+++ config/DBConfig.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: neel
+ * Date: 12/19/16
+ * Time: 8:47 PM
+ */
+
+//const DB_NAME="testDb";
+//const HOST_NAME="127.0.0.1";
+//const USER_NAME="root";
+//const PASSWORD='';
+
+
+const DB_NAME="heroku_ff1b64fdff3b77e";
+const HOST_NAME="us-cdbr-iron-east-04.cleardb.net";
+const USER_NAME="bf6aec454b0ed0";
+const PASSWORD='f502bcd9';
